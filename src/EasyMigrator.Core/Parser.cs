25081a4858da14e5c0f5c44e8fa39c54a4c6ee22
Quentin Starin <quentin@screenfeed.com>
2014-04-28 16:52:50 -0500

    moved parser context and conventions to new file

--- src/EasyMigrator.Core/Parser.cs
+++ src/EasyMigrator.Core/Parser.cs
@@ -4,32 +4,11 @@ using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml;
-using System.Xml.Linq;
 using EasyMigrator.Model;
 
 
 namespace EasyMigrator.Parsing
 {
-    public class Conventions
-    {
-        public Func<Context, string> TableName { get; set; }
-        public Func<Context, Column> PrimaryKey { get; set; }
-        public Func<Context, string> PrimaryKeyName { get; set; }
-        public ITypeMap TypeMap { get; set; }
-    }
-
-    public class Context
-    {
-        public Conventions Conventions { get; set; }
-        public Table Table { get; set; }
-        public Type TableType { get; set; }
-        public IEnumerable<FieldInfo> Fields { get; set; }
-        public object Model { get; set; }
-    }
-
-
     public class Parser
     {
         public static Parser Default { get { return _default.Value; } set { _default = new Lazy<Parser>(() => value); } }
@@ -37,44 +16,8 @@ namespace EasyMigrator.Parsing
 
         public Conventions Conventions { get; set; }
 
+        public Parser() : this(Conventions.Default) { }
         public Parser(Conventions conventions) { Conventions = conventions; }
-
-        public Parser()
-            : this(new Conventions {
-                TableName = c => Regex.Replace(c.TableType.Name, "Table$", ""),
-                PrimaryKey = c => new Column {
-                    Name = "Id",
-                    Type = DbType.Int32,
-                    IsIdentity = true,
-                    IsPrimaryKey = true
-                },
-                TypeMap = new TypeMap()
-                       .Add(new Dictionary<Type, DbType> {
-                           {typeof(sbyte), DbType.SByte},
-                           {typeof(byte), DbType.Byte},
-                           {typeof(short), DbType.Int16},
-                           {typeof(ushort), DbType.UInt16},
-                           {typeof(int), DbType.Int32},
-                           {typeof(uint), DbType.UInt32},
-                           {typeof(long), DbType.Int64},
-                           {typeof(ulong), DbType.UInt64},
-                           {typeof(bool), DbType.Boolean},
-                           {typeof(float), DbType.Single},
-                           {typeof(double), DbType.Double},
-                           {typeof(decimal), DbType.Currency},
-                           {typeof(DateTime), DbType.DateTime},
-                           {typeof(Guid), DbType.Guid},
-                           {typeof(byte[]), DbType.Binary},
-                           {typeof(XDocument), DbType.Xml},
-                           {typeof(XmlDocument), DbType.Xml}
-                       })
-                       .Add(typeof(string), 
-                               f => f.HasAttribute<AnsiAttribute>()
-                                   ? (f.HasAttribute<FixedLengthAttribute>() ? DbType.AnsiStringFixedLength : DbType.AnsiString)
-                                   : (f.HasAttribute<FixedLengthAttribute>() ? DbType.StringFixedLength : DbType.String)
-                       )
-            }) 
-        {}
 
 
         public Table ParseTable(Type type)
---
+++ src/EasyMigrator.Core/ParserContextAndConventions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using EasyMigrator.Model;
+
+
+namespace EasyMigrator.Parsing
+{
+    public class Context
+    {
+        public Conventions Conventions { get; set; }
+        public Table Table { get; set; }
+        public Type TableType { get; set; }
+        public IEnumerable<FieldInfo> Fields { get; set; }
+        public object Model { get; set; }
+    }
+
+    public class Conventions
+    {
+        public Func<Context, string> TableName { get; set; }
+        public Func<Context, Column> PrimaryKey { get; set; }
+        public Func<Context, string> PrimaryKeyName { get; set; }
+        public ITypeMap TypeMap { get; set; }
+
+        static public Conventions Default
+        {
+            get
+            {
+                return new Conventions {
+                    TableName = c => Regex.Replace(c.TableType.Name, "Table$", ""),
+                    PrimaryKey = c => new Column {
+                        Name = "Id",
+                        Type = DbType.Int32,
+                        IsIdentity = true,
+                        IsPrimaryKey = true
+                    },
+                    TypeMap = new TypeMap()
+                        .Add(new Dictionary<Type, DbType> {
+                            {typeof(sbyte), DbType.SByte},
+                            {typeof(byte), DbType.Byte},
+                            {typeof(short), DbType.Int16},
+                            {typeof(ushort), DbType.UInt16},
+                            {typeof(int), DbType.Int32},
+                            {typeof(uint), DbType.UInt32},
+                            {typeof(long), DbType.Int64},
+                            {typeof(ulong), DbType.UInt64},
+                            {typeof(bool), DbType.Boolean},
+                            {typeof(float), DbType.Single},
+                            {typeof(double), DbType.Double},
+                            {typeof(decimal), DbType.Currency},
+                            {typeof(DateTime), DbType.DateTime},
+                            {typeof(Guid), DbType.Guid},
+                            {typeof(byte[]), DbType.Binary},
+                            {typeof(XDocument), DbType.Xml},
+                            {typeof(XmlDocument), DbType.Xml}
+                        })
+                        .Add(typeof(string),
+                                f => f.HasAttribute<AnsiAttribute>()
+                                    ? (f.HasAttribute<FixedLengthAttribute>() ? DbType.AnsiStringFixedLength : DbType.AnsiString)
+                                    : (f.HasAttribute<FixedLengthAttribute>() ? DbType.StringFixedLength : DbType.String)
+                        )
+                };
+            }
+        }
+    }
+}
