b2153bf112623a7d218fac1c96b0a6ef0df89eec
Quentin Starin <quentin@screenfeed.com>
2014-04-28 17:05:12 -0500

    added unit test project

---
+++ src/EasyMigrator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EasyMigrator.Tests")]
+[assembly: AssemblyDescription("Unit tests for EasyMigrator.")]
---
+++ src/EasyMigrator.Tests/TableParser/Table1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace EasyMigrator.Tests.TableParser
+{
+    [TestFixture]
+    public class Table1
+    {
+        [Test]
+        public void Parses()
+        {
+            var table = Parsing.Parser.Default.ParseTable(typeof(Table1Table));
+            Assert.AreEqual("Table1", table.Name);
+        }
+
+        public class Table1Table
+        {
+        }
+    }
+}
