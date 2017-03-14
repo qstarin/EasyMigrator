595d6e6a41d89a3bc9d9dbc39b1ef283ba051560
Quentin <quentin.starin@gmail.com>
2017-03-14 17:09:12 -0500

    starting to add some index creation extensions

---
+++ src/EasyMigrator.MigratorDotNet/IndexExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Migrator.Framework;
+
+
+namespace EasyMigrator.MigratorDotNet
+{
+    static public class IndexExtensions
+    {
+        static private void AddIndex(this ITransformationProvider Database, string table, string indexName, params string[] columnsWithDirection)
+            => Database.AddIndex(table, indexName, false, false, columnsWithDirection);
+
+        static private void AddIndex(this ITransformationProvider Database, string table, string indexName, bool unique, params string[] columnsWithDirection)
+            => Database.AddIndex(table, indexName, unique, false, columnsWithDirection);
+
+        static private void AddIndex(this ITransformationProvider Database, string table, string indexName, bool unique, bool clustered, params string[] columnsWithDirection)
+            => Database.ExecuteNonQuery($"CREATE {(unique ? "UNIQUE " : "")}{(clustered ? "CLUSTERED" : "NONCLUSTERED")} INDEX {indexName} ON [{table}] ({string.Join(", ", columnsWithDirection)})");
+    }
+}
--- src/EasyMigrator.MigratorDotNet/PrimaryKeyExtensions.cs
+++ src/EasyMigrator.MigratorDotNet/PrimaryKeyExtensions.cs
@@ -11,21 +11,21 @@ namespace EasyMigrator.MigratorDotNet
 {
     static public class PrimaryKeyExtensions
     {
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, params Expression<Func<TTable, object>>[] columns) => db.AddPrimaryKey(true, Parsing.Parser.Default, columns);
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns) => db.AddPrimaryKey(true, parser, columns);
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, bool clustered, params Expression<Func<TTable, object>>[] columns) => db.AddPrimaryKey(null, clustered, Parsing.Parser.Default, columns);
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, bool clustered, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns) => db.AddPrimaryKey(null, clustered, parser, columns);
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, string constraintName, bool clustered, params Expression<Func<TTable, object>>[] columns) => db.AddPrimaryKey(constraintName, clustered, Parsing.Parser.Default, columns);
-        static public void AddPrimaryKey<TTable>(this ITransformationProvider db, string constraintName, bool clustered, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns)
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, params Expression<Func<TTable, object>>[] columns) => Database.AddPrimaryKey(true, Parsing.Parser.Default, columns);
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns) => Database.AddPrimaryKey(true, parser, columns);
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, bool clustered, params Expression<Func<TTable, object>>[] columns) => Database.AddPrimaryKey(null, clustered, Parsing.Parser.Default, columns);
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, bool clustered, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns) => Database.AddPrimaryKey(null, clustered, parser, columns);
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, string constraintName, bool clustered, params Expression<Func<TTable, object>>[] columns) => Database.AddPrimaryKey(constraintName, clustered, Parsing.Parser.Default, columns);
+        static public void AddPrimaryKey<TTable>(this ITransformationProvider Database, string constraintName, bool clustered, Parsing.Parser parser, params Expression<Func<TTable, object>>[] columns)
         {
             var context = parser.ParseTableType(typeof(TTable));
             var colNames = columns.Select(e => e.GetExpressionField()).Select(fi => context.Columns[fi].Name);
-            db.AddPrimaryKey(context.Table.Name, constraintName ?? context.Table.PrimaryKeyName, clustered, colNames.ToArray());
+            Database.AddPrimaryKey(context.Table.Name, constraintName ?? context.Table.PrimaryKeyName, clustered, colNames.ToArray());
         }
-        static public void AddPrimaryKey(this ITransformationProvider db, string table, params string[] columns) => db.AddPrimaryKey(table, true, columns);
-        static public void AddPrimaryKey(this ITransformationProvider db, string table, bool clustered, params string[] columns) => db.AddPrimaryKey(table, null, clustered, columns);
-        static public void AddPrimaryKey(this ITransformationProvider db, string table, string constraintName, bool clustered, params string[] columns)
+        static public void AddPrimaryKey(this ITransformationProvider Database, string table, params string[] columns) => Database.AddPrimaryKey(table, true, columns);
+        static public void AddPrimaryKey(this ITransformationProvider Database, string table, bool clustered, params string[] columns) => Database.AddPrimaryKey(table, null, clustered, columns);
+        static public void AddPrimaryKey(this ITransformationProvider Database, string table, string constraintName, bool clustered, params string[] columns)
 
-            => db.ExecuteNonQuery($"ALTER TABLE [{table}] ADD CONSTRAINT {(constraintName ?? $"PK_{table}")} PRIMARY KEY {(clustered ? "CLUSTERED" : "NONCLUSTERED")} ({string.Join(", ", columns)})");
+            => Database.ExecuteNonQuery($"ALTER TABLE [{table}] ADD CONSTRAINT {(constraintName ?? $"PK_{table}")} PRIMARY KEY {(clustered ? "CLUSTERED" : "NONCLUSTERED")} ({string.Join(", ", columns)})");
     }
 }
