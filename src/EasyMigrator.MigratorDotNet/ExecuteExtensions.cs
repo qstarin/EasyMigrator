3b2be72812a5ae588eed69df584d110dd4719420
Quentin <quentin.starin@gmail.com>
2017-03-18 18:46:39 -0500

    execute with connection extension for MigratorDotNet

---
+++ src/EasyMigrator.MigratorDotNet/ExecuteExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Migrator.Framework;
+
+
+namespace EasyMigrator
+{
+    static public class ExecuteExtensions
+    {
+        static public void ExecuteWithConnection(this ITransformationProvider Database, Action<IDbConnection, IDbTransaction> operation)
+        {
+            var cmd = Database.GetCommand();
+            operation(cmd.Connection, cmd.Transaction);
+        }
+    }
+}
