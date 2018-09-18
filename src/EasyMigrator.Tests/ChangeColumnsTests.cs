3dca80d42ea2e918965c5e075711d9156de23ec5
Quentin <quentin.starin@gmail.com>
2018-09-18 13:26:36 -0500

    ChangeColumns test for MigratorDotNet

---
+++ src/EasyMigrator.Tests/ChangeColumnsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasyMigrator.Tests.Integration;
+using NUnit.Framework;
+
+
+namespace EasyMigrator.Tests.MigratorDotNet
+{
+    [TestFixture]
+    public class ChangeColumns : IntegrationTestBase
+    {
+        public ChangeColumns() : base(s => new Integration.MigratorDotNet.Migrator(s)) { }
+
+        [Test]
+        public void Content() => Test<Schemas.ChangeColumns.Content>(
+            (testCase, migrations) => {
+                AddMigrations(testCase, migrations);
+
+                migrations.AddMigrationForMigratorDotNet(
+                    m => {
+                        m.Database.ChangeColumns<Schemas.ChangeColumns.Content.ColumnsToChange>();
+                    },
+                    m => {
+                        m.Database.ChangeColumns<Schemas.ChangeColumns.Content.OriginalColumns>();
+                    });
+            }
+        );
+    }
+}
+
---
+++ src/EasyMigrator.Tests/Schemas/ChangeColumns.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using EasyMigrator.Parsing.Model;
+
+
+namespace EasyMigrator.Tests.Schemas
+{
+    public class ChangeColumns
+    {
+        public class Content
+        { 
+            [NPoco.TableName("Content")]
+            public class Poco
+            {
+                [Max] public string Title;
+                [Medium] public string ExternalId;
+                public DateTime DisplayStartDate;
+                public DateTime? DisplayEndDate;
+            }
+
+            [Name("Content")]
+            public class ColumnsToChange
+            {
+                [Precision(2)] public DateTime DisplayStartDate;
+                [Precision(2)] public DateTime? DisplayEndDate;
+            }
+
+            [Name("Content")]
+            public class OriginalColumns
+            {
+                public DateTime DisplayStartDate;
+                public DateTime? DisplayEndDate;
+            }
+
+            static Table Model = new Table {
+                Name = "Content",
+                Columns = new[] {
+                    new Column {
+                        Name = "Id",
+                        Type = DbType.Int32,
+                        IsPrimaryKey = true,
+                        AutoIncrement = new AutoIncAttribute()
+                    },
+                    new Column {
+                        Name = "Title",
+                        Type = DbType.String,
+                        Length = int.MaxValue,
+                        IsNullable = true,
+                    },
+                    new Column {
+                        Name = "ExternalId",
+                        Type = DbType.String,
+                        Length = 255,
+                        IsNullable = true,
+                    },
+                    new Column {
+                        Name = "DisplayStartDate",
+                        Type = DbType.DateTime2,
+                        Precision = new PrecisionAttribute(2),
+                    },
+                    new Column {
+                        Name = "DisplayEndDate",
+                        IsNullable = true,
+                        Type = DbType.DateTime2,
+                        Precision = new PrecisionAttribute(2),
+                    },
+                }
+            };
+        }
+    }
+}
