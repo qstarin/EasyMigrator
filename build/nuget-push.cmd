c6ad2eb647f8dd65f19c244b026a91318ba652d8
Quentin <quentin.starin@gmail.com>
2019-07-14 21:20:06 -0500

    remove symbol packages from push cmd

--- build/nuget-push.cmd
+++ build/nuget-push.cmd
@@ -4,8 +4,5 @@ del /f /q *.nupkg
 call nuget-pack.cmd
 
 ..\src\.nuget\nuget.exe push EasyMigrator.Core.?.?.?.nupkg %1
-..\src\.nuget\nuget.exe push EasyMigrator.Core.?.?.?.symbols.nupkg %1
 ..\src\.nuget\nuget.exe push EasyMigrator.FluentMigrator.?.?.?.nupkg %1
-..\src\.nuget\nuget.exe push EasyMigrator.FluentMigrator.?.?.?.symbols.nupkg %1
 ..\src\.nuget\nuget.exe push EasyMigrator.MigratorDotNet.?.?.?.nupkg %1
-..\src\.nuget\nuget.exe push EasyMigrator.MigratorDotNet.?.?.?.symbols.nupkg %1
