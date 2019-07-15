fcd56d0e582d45f6c61f567cdb8a8b1e21819fb0
Quentin <quentin.starin@gmail.com>
2019-07-14 21:19:08 -0500

    don't build symbol packages since they aren't going anywhere

--- build/nuget-pack.cmd
+++ build/nuget-pack.cmd
@@ -1,3 +1,3 @@
-..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.Core\EasyMigrator.Core.csproj -sym
-..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.FluentMigrator\EasyMigrator.FluentMigrator.csproj -sym
-..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.MigratorDotNet\EasyMigrator.MigratorDotNet.csproj -sym
+..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.Core\EasyMigrator.Core.csproj
+..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.FluentMigrator\EasyMigrator.FluentMigrator.csproj
+..\src\.nuget\nuget.exe pack ..\src\EasyMigrator.MigratorDotNet\EasyMigrator.MigratorDotNet.csproj
